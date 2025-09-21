28f3bd9df38f75a3deffb57438202441a1da3e6a
ferlicea2004 <200810010@ucc.mx>
2025-09-20 22:20:02 -0600

    Update splashscreen.java

--- splashscreen.java
+++ splashscreen.java
@@ -1,24 +1,46 @@
 package com.example.splashscreen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class activity_principal extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+
+  Animation tyong1, tyong2;
+  ImageView imgTyong1, imgTyong2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_principal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+
+        tyong1= AnimationUtils.loadAnimation(this,R.anim.tyong1_anim);
+        tyong2= AnimationUtils.loadAnimation(this,R.anim.tyong2_anim);
+        imgTyong1=findViewById(R.id.p1);
+        imgTyong2=findViewById(R.id.p2);
+
+        imgTyong1.startAnimation(tyong1);
+        imgTyong2.startAnimation(tyong2);
+
+      Handler handler= new Handler();
+      handler.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+          startActivity(new Intent(MainActivity.this, activity_principal.class));
+
+          finish();
+        }
+      },1500);
+
+
+
+    }
 }
